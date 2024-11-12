e71f2ad4f03034d01d32cf3d1495d8ff9677f279
Rajnika Ghosh <rajnikag6@gmail.com>
2024-11-12 08:17:18 +0530

    Bubble Sort

---
+++ BubbleSort.java
@@ -0,0 +1,36 @@
+public class BubbleSort {
+    public static int[] BubbleSort(int[] arr)
+        {
+               int n = arr.length;
+                int i = 0;
+                boolean swapped = false;
+                for(i = 0;i<n-i;i++)
+                {
+                    
+                    for(int j = 0;  j  < n-i-1 ;j++)
+                    {
+                        if(arr[j]>arr[j +1])
+                        {
+                            int temp = arr[j];
+                            arr[j]= arr[j+1];
+                            arr[j+1]= temp;
+                            swapped = true;
+                        }
+                        
+                    }
+                    if (swapped == false)
+                        break;
+                    
+                }
+                return arr;
+            
+        }
+        static public void main(String args[]){  
+            int[] arr = {1,2,3,4,5};
+            int[] ans = BubbleSort(arr);
+            for (int p : ans) {
+                System.out.println(p);  
+            }
+                
+            }
+}
